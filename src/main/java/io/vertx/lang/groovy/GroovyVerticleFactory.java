878d887c0586fca70d66364f8727a6c80c7718f8
Julien Viet <julien@julienviet.com>
2014-07-22 13:37:33 +0200

    Use eclipse groovy compiler

--- src/main/java/io/vertx/lang/groovy/GroovyVerticleFactory.java
+++ src/main/java/io/vertx/lang/groovy/GroovyVerticleFactory.java
@@ -1,9 +1,15 @@
 package io.vertx.lang.groovy;
 
+import io.vertx.groovy.core.Context;
+
 /**
  * Placeholder
  *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public class GroovyVerticleFactory {
+
+  // Just here to see if it compiles
+  private Context context = null;
+
 }
