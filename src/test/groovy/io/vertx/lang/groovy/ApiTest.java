967b6d46f833da3fb52b3f67ec4bd2f46cf2e1d6
Julien Viet <julien@julienviet.com>
2014-07-25 12:07:30 +0200

    Handle basic handler types

--- src/test/groovy/io/vertx/lang/groovy/ApiTest.java
+++ src/test/groovy/io/vertx/lang/groovy/ApiTest.java
@@ -1,8 +1,14 @@
 package io.vertx.lang.groovy;
 
 import io.vertx.codegen.testmodel.TestInterfaceImpl;
+import io.vertx.core.Handler;
 import io.vertx.groovy.codegen.testmodel.TestInterface;
 import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
@@ -21,8 +27,47 @@ public class ApiTest {
     obj.methodWithBasicBoxedParams((byte) 123, (short) 12345, 1234567, 1265615234l, 12.345f, 12.34566d, true, 'X');
   }
 
-//  @Test
-//  public void methodWithHandlerBasicTypes() {
-    // obj.methodWithHandlerBasicTypes();
-//  }
+  @Test
+  public void methodWithHandlerBasicTypes() {
+    AtomicInteger count = new AtomicInteger();
+    obj.methodWithHandlerBasicTypes(
+        event -> {
+          assertEquals(123, (byte) event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(12345, (short) event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(1234567, (int) event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(1265615234l, (long) event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(12.345f, (float) event, 0);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(12.34566d, (double) event, 0);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals(true, event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals('X', (char) event);
+          count.incrementAndGet();
+        },
+        event -> {
+          assertEquals("quux!", event);
+          count.incrementAndGet();
+        }
+    );
+    assertEquals(9, count.get());
+  }
 }
