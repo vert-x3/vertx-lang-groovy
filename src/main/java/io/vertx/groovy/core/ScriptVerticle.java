aba8a1d64dfd977bfe3d7d7323b2a2d0b617d9e0
Julien Viet <julien@julienviet.com>
2014-08-01 17:08:52 +0200

    A bit of doc

--- src/main/java/io/vertx/groovy/core/ScriptVerticle.java
+++ src/main/java/io/vertx/groovy/core/ScriptVerticle.java
@@ -6,6 +6,15 @@ import groovy.lang.Script;
 import io.vertx.core.AbstractVerticle;
 
 /**
+ * A Vert.x native verticle wrapping a Groovy script, the script will be executed when the Verticle starts.
+ * When the script defines a no arg accessible <code>vertxStop</code> method, this method will be invoked
+ * when the verticle stops. Before the script starts the following objects are bound in the script binding:
+ * <ul>
+ *   <li><code>vertx</code>: the {@link Vertx} object</li>
+ *   <li><code>deploymentID</code>: the deploymentID of this Verticle</li>
+ *   <li><code>config</code>: the Verticle config as a <code>Map&lt;String, Object&gt;</code></li>
+ * </ul>
+ *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 public class ScriptVerticle extends AbstractVerticle {
