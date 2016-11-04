55abf52f34e1a25b7bb30c618010ee6f562b829a
Julien Viet <julien@julienviet.com>
2016-11-04 13:19:54 +0100

    Example should be in Groovy and not in Java

--- src/main/java/io/vertx/lang/groovy/GroovyDocGenerator.java
+++ src/main/java/io/vertx/lang/groovy/GroovyDocGenerator.java
@@ -15,13 +15,73 @@
  */
 package io.vertx.lang.groovy;
 
+import io.vertx.codetrans.CodeTranslator;
+import io.vertx.codetrans.lang.groovy.GroovyLang;
+import io.vertx.docgen.Coordinate;
+import io.vertx.docgen.DocGenerator;
 import io.vertx.docgen.JavaDocGenerator;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
-public class GroovyDocGenerator extends JavaDocGenerator {
+public class GroovyDocGenerator implements DocGenerator {
+
+  private CodeTranslator translator;
+  private JavaDocGenerator javaGen = new JavaDocGenerator();
 
   public GroovyDocGenerator() {
+  }
+
+  @Override
+  public void init(ProcessingEnvironment env) {
+    translator = new CodeTranslator(env);
+    javaGen.init(env);
+  }
+
+  @Override
+  public String renderSource(ExecutableElement elt, String source) {
+    GroovyLang lang = new GroovyLang();
+    try {
+      return translator.translate(elt, lang);
+    } catch (Exception e) {
+      System.out.println("Cannot generate " + elt.getEnclosingElement().getSimpleName() + "#" + elt.getSimpleName() + " : " + e.getMessage());
+      return "Code not translatable";
+    }
+  }
+
+  @Override
+  public String getName() {
+    return "groovy";
+  }
+
+  @Override
+  public String resolveTypeLink(TypeElement elt, Coordinate coordinate) {
+    return javaGen.resolveTypeLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveConstructorLink(ExecutableElement elt, Coordinate coordinate) {
+    return javaGen.resolveConstructorLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveMethodLink(ExecutableElement elt, Coordinate coordinate) {
+    return javaGen.resolveMethodLink(elt, coordinate);
+  }
+
+  @Override
+  public String resolveLabel(Element elt, String defaultLabel) {
+    return javaGen.resolveLabel(elt, defaultLabel);
+  }
+
+  @Override
+  public String resolveFieldLink(VariableElement elt, Coordinate coordinate) {
+    return javaGen.resolveFieldLink(elt, coordinate);
   }
 }
