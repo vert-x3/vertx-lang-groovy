576207e9447fa8f44413b9e4ae5a18079bf98e7f
Julien Viet <julien@julienviet.com>
2018-02-28 16:35:23 +0100

    Split vertx-lang-groovy into vertx-lang-groovy-codegen and vertx-lang-groovy

--- vertx-lang-groovy-codegen/src/test/resources/io/vertx/lang/groovy/ResolveVertxVerticleScript.groovy
+++ vertx-lang-groovy-codegen/src/test/resources/io/vertx/lang/groovy/ResolveVertxVerticleScript.groovy
@@ -1,7 +1,8 @@
+import io.vertx.core.json.JsonObject
 import io.vertx.groovy.core.Vertx
 
 // Make sure they have the correct type
 Vertx vertx = (Vertx) vertx;
 String deploymentID = vertx.currentContext().deploymentID()
-Map<String, Object> config = vertx.currentContext().config()
+JsonObject config = vertx.currentContext().config()
 System.setProperty("started", "${vertx != null && deploymentID != null && config != null}");
