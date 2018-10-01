52b1b811f8bdae17fd6f7b46e1b3123a2de7d27e
Julien Viet <julien@julienviet.com>
2018-10-01 15:56:59 +0200

    Revert "Generation does not happen here anymore"
    This reverts commit c58923378ccc175fd332b08456292a2ed685c525.

---
+++ vertx-lang-groovy/src/test/groovy/io/vertx/groovy/unit/GroovyJUnitTest.groovy
@@ -0,0 +1,18 @@
+package io.vertx.groovy.unit
+
+import io.vertx.ext.unit.TestContext
+import io.vertx.ext.unit.junit.VertxUnitRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+@RunWith(VertxUnitRunner.class)
+class GroovyJUnitTest {
+
+  @Test
+  public void testFoo(TestContext context) {
+
+  }
+}
