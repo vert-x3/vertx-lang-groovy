bc9c359d24b8f1ae0da6b0cda1f6fd246ce6bf8e
aaloise <aaloise@gmail.com>
2018-10-25 09:40:06 -0400

    Update VertxExtensionMethodBoostrap.java

--- vertx-lang-groovy-gen/src/main/java/io/vertx/lang/groovy/VertxExtensionMethodBoostrap.java
+++ vertx-lang-groovy-gen/src/main/java/io/vertx/lang/groovy/VertxExtensionMethodBoostrap.java
@@ -45,6 +45,7 @@ public class VertxExtensionMethodBoostrap {
               ScanResult result = new ClassGraph().verbose().enableAllInfo().whitelistPackages("io.vertx.groovy.core").scan();
               ClassInfoList infolist = result.getSubclasses("org.codehaus.groovy.runtime.m12n.ExtensionModule").directOnly();
               ClassInfo info = infolist.get("io.vertx.groovy.core.VertxExtensionModule");                            
+              System.out.println(info);
               /*ExtensionModule module = info.loadClass().getDeclaringClass().newInstance();
               if (!moduleRegistry.hasModule(module.getName())) {
                 moduleRegistry.addModule(module);
