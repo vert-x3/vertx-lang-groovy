16bbf23088c9cda36cf0349342506b3272e5c031
aaloise <aaloise@gmail.com>
2020-07-23 13:55:36 -0400

    Missing import

--- vertx-lang-groovy-gen/src/main/java/io/vertx/lang/groovy/VertxTransformation.java
+++ vertx-lang-groovy-gen/src/main/java/io/vertx/lang/groovy/VertxTransformation.java
@@ -50,6 +50,7 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
+import java.util.Iterator;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
