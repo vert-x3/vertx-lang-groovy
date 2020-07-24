cf2e5b04d2531939509d96eed0ec64968274a84f
Julien Viet <julien@julienviet.com>
2020-07-24 14:41:54 +0200

    Simplify build and remove doc generation, consolidate existing doc into a new documentation - fixes #108

--- src/main/java/io/vertx/lang/groovy/VertxExtensionModule.java
+++ src/main/java/io/vertx/lang/groovy/VertxExtensionModule.java
@@ -9,8 +9,22 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
+/**
+ * Extension module for integrating Vert.x common types with Groovy.
+ */
 public class VertxExtensionModule {
 
+  /**
+   * Convert a JSON like map data structure to a Vert.x data object or a {@link JsonObject}.
+   *
+   * <p> The data object is a class annotated by {@link DataObject} that provides a public constructor
+   * with a {@link JsonObject} argument.
+   *
+   * @param map the JSON like map like data structure
+   * @param clazz the target type which can be a Java class annotated with {@link DataObject} and provides a {@link JsonObject}
+   *              constructor, or {@link JsonObject}
+   * @return an instance of the specified {@code clazz}
+   */
   public static Object asType(Map map, Class clazz) throws Throwable {
     if (clazz == JsonObject.class) {
       return new JsonObject(map);
@@ -30,16 +44,38 @@ public class VertxExtensionModule {
     return null;
   }
 
+  /**
+   * Subscript operator for {@link JsonObject}, the read part.
+   *
+   * @param json the json object
+   * @param key the key
+   * @return the json value for {@code key}
+   */
   public static Object getAt(JsonObject json, String key) {
     return json.getValue(key);
   }
 
+  /**
+   * Subscript operator for {@link JsonObject}, the write part.
+   *
+   * @param json the json object
+   * @param key the key
+   * @param value the json value
+   * @return the previous json value for {@code key}
+   */
   public static Object putAt(JsonObject json, String key, Object value) {
     Object prev = json.getValue(key);
     json.put(key, value);
     return prev;
   }
 
+  /**
+   * Subscript operator for {@link JsonArray}, the read part.
+   *
+   * @param json the json array
+   * @param idx the idx
+   * @return the json value for {@code idx}
+   */
   public static Object getAt(JsonArray json, int idx) {
     int size = json.size();
     if (idx < 0) {
@@ -55,6 +91,12 @@ public class VertxExtensionModule {
     }
   }
 
+  /**
+   * {@code <<} operator for {@link JsonObject}.
+   *
+   * @param json the json object
+   * @param value the json value
+   */
   public static void leftShift(JsonArray json, Object value) {
     json.add(value);
   }
