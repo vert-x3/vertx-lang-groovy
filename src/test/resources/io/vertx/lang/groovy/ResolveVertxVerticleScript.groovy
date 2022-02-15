4f849d73cf596918ab53c341a135c7792f64313d
aaloise <aaloise@gmail.com>
2022-02-15 18:44:42 -0400

    Update ResolveVertxVerticleScript.groovy

--- src/test/resources/io/vertx/lang/groovy/ResolveVertxVerticleScript.groovy
+++ src/test/resources/io/vertx/lang/groovy/ResolveVertxVerticleScript.groovy
@@ -3,6 +3,6 @@ import io.vertx.core.Vertx
 
 // Make sure they have the correct type
 Vertx vertx = (Vertx) vertx;
-String deploymentID = vertx.getOrCreateContext.deploymentID()
+String deploymentID = vertx.getOrCreateContext().deploymentID()
 JsonObject config = vertx.getOrCreateContext().config()
 System.setProperty("started", "${vertx != null && deploymentID != null && config != null}");
